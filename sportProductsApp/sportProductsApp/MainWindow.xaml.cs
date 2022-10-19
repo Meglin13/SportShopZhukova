16368c4d434c8d6157f7d2c318d83e0960952c85
Lisavina Alena <16alena161003@gmail.com>
2022-10-19 10:06:56 +0300

    add styles

--- sportProductsApp/sportProductsApp/MainWindow.xaml.cs
+++ sportProductsApp/sportProductsApp/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace sportProductsApp
             InitializeComponent();
 
             Manager.MainFrame = MainFrame;
-            Manager.MainFrame.Navigate(new Pages.ManagerAndAuthUserPage());
+            Manager.MainFrame.Navigate(new Pages.LoginPage());
         }
     }
 }
